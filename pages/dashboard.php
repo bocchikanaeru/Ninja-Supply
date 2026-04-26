da4ac5897d06a72d36920e1f633c8cb55841ebb9
bocchikanaeru <bocchikanaeru@gmail.com>
2026-04-26 15:56:44 +0700

    Delete pages directory

--- pages/dashboard.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '../helpers/auth.php';
-
-checkLogin();
-
-$target = normalizeRole($_SESSION['role'] ?? '') === 'owner' ? 'laporan.php' : 'stok.php';
-header('Location: ' . $target);
-exit;
